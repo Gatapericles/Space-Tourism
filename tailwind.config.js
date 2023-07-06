cf3c963eae9e2576f8bf9541b3807d2396271534
Samuel Martins <sm324477@gmail.com>
2023-07-06 14:05:03 -0300

    Fonte adicionada e configurando o tamanho da tela

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        BarlowCondensed: ["Barlow Condensed, sans-serif"],
+        Barlow: ["Barlow"],
+      },
+      screens: {
+        sm: "380px",
+        sm_md: "700px",
+        md: "770px",
+        lg: "976px",
+        xl: "1440px",
+      }
+    },
   },
   plugins: [],
 }
